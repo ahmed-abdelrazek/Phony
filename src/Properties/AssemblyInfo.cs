76a03c7ca790738500418e42fff293b8da8c58fc
Ahmed Mohamed Abdel-Razek <ahmed_master23@hotmail.com>
2018-06-19 09:13:42 +0200

    New Version

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -1,6 +1,5 @@
 ﻿using System.Reflection;
 using System.Resources;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 
@@ -8,7 +7,7 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Phony")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("An Arabic Mobile Store Management Application")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ShababConquer")]
 [assembly: AssemblyProduct("Phony")]
@@ -51,7 +50,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 [assembly: NeutralResourcesLanguage("ar-EG")]
 
